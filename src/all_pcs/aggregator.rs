47ea41c3eb8c73515779a3b4021e9a50c958ee1a
knewjade <knewjade@gmail.com>
2023-01-30 09:51:25 +0900

    WIP

--- src/all_pcs/aggregator.rs
+++ src/all_pcs/aggregator.rs
@@ -85,15 +85,13 @@ impl Aggregator {
             spawn_position: self.spawn_position,
         };
 
-        let filled = vec![Lines::blank(); self.clipped_board.height() as usize];
         let mut results = Vec::with_capacity((self.clipped_board.spaces() / 4) as usize);
-        self.aggregate_recursively(self.nodes.head_index_id().unwrap(), filled, &mut results, &checker)
+        self.aggregate_recursively(self.nodes.head_index_id().unwrap(), &mut results, &checker)
     }
 
     fn aggregate_recursively<'a>(
         &'a self,
         index_id: IndexId,
-        filled: Vec<Lines>,
         placed_pieces: &mut Vec<&'a PlacedPieceBlocks>,
         checker: &'a impl PcAggregationChecker,
     ) -> u64 {
@@ -106,70 +104,28 @@ impl Aggregator {
                 for item in item_ids {
                     let current = &self.map_placed_piece_blocks[&item.placed_piece];
 
-                    // 使っている行から、事前に揃えておく必要がある行を求める
-                    // （=placed_pieceの行を揃えるために、使うことができない行を取り出す。）
-                    // （他のミノの制約によって、いま注目している行を先に消さないと、そもそも置けないブロックが含まれている行）
-                    let filled_rows = current.placed_piece.ys.iter()
-                        .fold(Lines::blank(), |prev, &y| prev | filled[y as usize]);
-
-                    // 注目しているミノを置く前の絶対に揃えられないラインが削除されていないといけないか
-                    if current.intercepted_rows.overlaps(&filled_rows) {
-                        // 使っている
-                        continue;
-                    }
-
-                    // TODO 楽観的プレイスメントチェック
-                    // すべてがうまった想定のボードから、
-                    // 過去のピースのうち必ずカレントよりも後ろになるピースを抜いたものに対して配置できるかを確認する。
-                    //
-                    // もしかすると、現在のキーチェックも同時に行える可能性が高い
-
+                    let mut filled_rows = Lines::blank(); // currentより後に使われることが確定している行
+                    // 次に挿入する位置。依存関係があるピースが必ず後ろにくるようにする。
+                    // 依存関係がない場合は任意。つまり、「後ろにあるから、後で置く」が常に成り立つわけではないので注意
                     let mut inserted = placed_pieces.len();
                     for index in (0..placed_pieces.len()).rev() {
                         if placed_pieces[index].intercepted_rows.overlaps(&current.using_rows) {
                             // placed_pieceを置く前提となる行を、currentが使用している = placed_pieceはcurrentより先には置けない
                             inserted = index;
+
+                            // つまり、placed_pieceが使っている行を、currentより前に揃えることはできない
+                            filled_rows |= placed_pieces[index].using_rows;
                         }
+                    }
+
+                    if current.intercepted_rows.overlaps(&filled_rows) {
+                        // currentの後のピースで使われる行が消えていないと、currentが置けない場合は、絶対に配置できないのでスキップ
+                        continue;
                     }
 
                     placed_pieces.insert(inserted, current);
-                    let mut ok = true;
 
-                    {
-                        let index = inserted;
-                        let current = placed_pieces[index];
-
-                        let mut board = self.goal_board.clone();
-                        let mut unset = false;
-                        for &blocks in &placed_pieces[index+1..] {
-                            if blocks.intercepted_rows.overlaps(&current.using_rows) {
-                                blocks.unset_all(&mut board);
-                                unset = true;
-                            }
-                        }
-
-                        if unset {
-                            current.unset_all(&mut board);
-                            board.clear_lines_partially(current.intercepted_rows);
-
-                            let bl_location = current.placed_piece.bottom_left();
-                            let ground_placement = current.placed_piece.piece.with(bl(bl_location.x, bl_location.y));
-                            if !ground_placement.is_landing(&board) {
-                                ok = false;
-                            }
-                        }
-                    }
-
-                    if ok {
-                        // intercepted_rowsを先に揃えないと、消すことができない行をマッピング
-                        let next_filled = current.intercepted_rows.ys_iter()
-                            .fold(filled.clone(), |mut filled, y| {
-                                filled[y as usize] |= current.using_rows;
-                                filled
-                            });
-
-                        success += self.aggregate_recursively(item.next_index_id, next_filled, placed_pieces, checker);
-                    }
+                    success += self.aggregate_recursively(item.next_index_id, placed_pieces, checker);
 
                     placed_pieces.remove(inserted);
                 }
@@ -177,7 +133,7 @@ impl Aggregator {
                 success
             }
             IndexNode::ToNextIndex(next_index_id) => {
-                self.aggregate_recursively(*next_index_id, filled, placed_pieces, checker)
+                self.aggregate_recursively(*next_index_id, placed_pieces, checker)
             }
             IndexNode::Complete => {
                 let mut ok = true;
@@ -187,7 +143,7 @@ impl Aggregator {
 
                     let mut board = self.goal_board.clone();
                     let mut unset = false;
-                    for &blocks in &placed_pieces[index+1..] {
+                    for &blocks in &placed_pieces[index + 1..] {
                         if blocks.intercepted_rows.overlaps(&current.using_rows) {
                             blocks.unset_all(&mut board);
                             unset = true;
